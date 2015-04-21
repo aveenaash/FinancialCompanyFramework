c11763d98a7892b47176ed7ed6d842a1f061c272
addiati <dini.addiati@gmail.com>
2015-04-21 16:56:59 -0500

    implement observer pattern for transaction activity
    when certain transaction satisfy certain condition it will send email to
    the customer

---
+++ src/com/finco/framework/model/account/EmailPredicate.java
@@ -0,0 +1,17 @@
+package com.finco.framework.model.account;
+
+import com.finco.framework.search.IPredicate;
+
+public class EmailPredicate implements IPredicate<Double>{
+
+	public EmailPredicate(){
+		
+	}
+	
+	@Override
+	public boolean check(Double input) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
